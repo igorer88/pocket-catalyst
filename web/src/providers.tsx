740bb76acaa319eb9a54ff90436855eb995894dc
Igor Escalona <igorer88@users.noreply.github.com>
2025-10-01 03:50:33 -0400

    Fix translations

--- web/src/providers.tsx
+++ web/src/providers.tsx
@@ -1,15 +1,15 @@
-import { IntlProvider } from 'react-intl'
 import { HeroUIProvider } from '@heroui/react'
+import { IntlProvider } from 'react-intl'
 
 import enMessages from './locales/en.json'
 import esMessages from './locales/es.json'
 
-const messages = {
+const messages: Record<string, Record<string, any>> = {
   en: enMessages,
   es: esMessages
 }
 
-const getLocale = () => {
+const getLocale = (): string => {
   const lang = navigator.language.split(/[-_]/)[0]
   return lang in messages ? lang : 'en'
 }
