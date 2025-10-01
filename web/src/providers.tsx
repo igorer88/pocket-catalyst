1e52201dfa4be1d8e4f4a4aaad7314706a817662
Igor Escalona <igorer88@users.noreply.github.com>
2025-10-01 03:29:12 -0400

    Config providers

--- web/src/providers.tsx
+++ web/src/providers.tsx
@@ -1,5 +1,25 @@
+import { IntlProvider } from 'react-intl'
 import { HeroUIProvider } from '@heroui/react'
 
+import enMessages from './locales/en.json'
+import esMessages from './locales/es.json'
+
+const messages = {
+  en: enMessages,
+  es: esMessages
+}
+
+const getLocale = () => {
+  const lang = navigator.language.split(/[-_]/)[0]
+  return lang in messages ? lang : 'en'
+}
+
+const locale = getLocale()
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </IntlProvider>
+  )
 }
